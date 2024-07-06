39add55ccbdbe6aabd312afb57192b61680b53e2
GracikaJain <99799236+GracikaJain@users.noreply.github.com>
2024-07-07 01:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 import pickle
 import pandas as pd
 
@@ -25,3 +25,44 @@ if st.button('Recommend'):
     rec = recommend(selected_movie_name)
     for i in rec:
         st.write(i)
+'''
+import streamlit as st
+import pickle
+import pandas as pd
+import os
+
+def recommend(movie):
+    movie_index = movies_df[movies_df['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movie_list:
+        movie_id = i[0]
+        recommended_movies.append(movies_df.iloc[movie_id].title)
+
+    return recommended_movies
+
+st.title('Movie Recommender System')
+
+# Ensure the files are in the same directory as this script
+current_dir = os.path.dirname(__file__)
+movies_path = os.path.join(current_dir, 'movies.pkl')
+similarity_path = os.path.join(current_dir, 'similarity.pkl')
+
+try:
+    movies_df = pickle.load(open(movies_path, 'rb'))
+    similarity = pickle.load(open(similarity_path, 'rb'))
+except FileNotFoundError as e:
+    st.error(f"File not found: {e.filename}. Please ensure the file is in the correct directory.")
+    st.stop()
+
+movies_list = movies_df['title'].values
+
+selected_movie_name = st.selectbox('Select the movie!', movies_list)
+
+if st.button('Recommend'):
+    rec = recommend(selected_movie_name)
+    for i in rec:
+        st.write(i)
+
